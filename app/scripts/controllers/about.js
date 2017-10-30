324192d3ac6b0286f21c5772d7f053510196a446
Bagya <bagya@mthoughtz.com>
2017-10-30 19:36:05 +0530

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testtodoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testtodoApp
+ */
+angular.module('testtodoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testtodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testtodoApp
+ */
+angular.module('testtodoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
